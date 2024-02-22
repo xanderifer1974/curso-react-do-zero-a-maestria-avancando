1287aa1a4e38da5df61cb6b1f7bc0243ee8587ad
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2024-02-22 19:40:18 -0300

    A propriedade Key

--- src/components/ListRender.jsx
+++ src/components/ListRender.jsx
@@ -9,12 +9,29 @@ const ListRender = () => {
     "Gabriel",
     "Gael",
   ]);
+
+  const [users] = useState([
+    { id: 1, name: "Alexandre", age: 49 },
+    { id: 2, name: "Rafael", age: 35 },
+    { id: 3, name: "João", age: 20 },
+    { id: 4, name: "Maria", age: 55 },
+    { id: 5, name: "Antonio", age: 33 },
+  ]);
   return (
-    <ul>
-      {list.map((item) => (
-        <li>{item}</li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {list.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.age}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
