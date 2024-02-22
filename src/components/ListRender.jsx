90604e9a710e4a56a918dca78882559959ed8905
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2024-02-22 20:11:54 -0300

    PreviousState

--- src/components/ListRender.jsx
+++ src/components/ListRender.jsx
@@ -10,13 +10,23 @@ const ListRender = () => {
     "Gael",
   ]);
 
-  const [users] = useState([
+  const [users, setUsers] = useState([
     { id: 1, name: "Alexandre", age: 49 },
     { id: 2, name: "Rafael", age: 35 },
     { id: 3, name: "João", age: 20 },
     { id: 4, name: "Maria", age: 55 },
     { id: 5, name: "Antonio", age: 33 },
   ]);
+
+  const deleteRandom = () => {
+    const randomNumber = Math.floor(Math.random() * 4);
+
+    setUsers((prevUsers) => {
+        return prevUsers.filter((user) => randomNumber !== user.id);
+    });
+};
+
+
   return (
     <div>
       <ul>
@@ -31,6 +41,7 @@ const ListRender = () => {
           </li>
         ))}
       </ul>
+      <button onClick={deleteRandom}>Delete Random User</button>
     </div>
   );
 };
