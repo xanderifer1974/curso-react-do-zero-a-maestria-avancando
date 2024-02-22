87780452ec46bacd40bb6584f9cea2904660e0cf
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2024-02-22 17:38:55 -0300

    Acessando imagens no public

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
      <h1>Avancando no React</h1>
-     
+     {/*Imagem em Public */}
+     <img src="/developer.png" alt="Developer" />
     </div>
   );
 }
