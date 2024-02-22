c2fdcfefbce29ba837d8fa8b8dcdd982563847b8
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2024-02-22 18:11:15 -0300

    Imagens no SRC

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import City from './asset/city.png'
 
 function App() {
   return (
@@ -6,6 +7,11 @@ function App() {
      <h1>Avancando no React</h1>
      {/*Imagem em Public */}
      <img src="/developer.png" alt="Developer" />
+
+     {/*Imagens em Asset */}
+     <div>
+      <img src={City} alt="Cidade" />
+     </div>
     </div>
   );
 }
