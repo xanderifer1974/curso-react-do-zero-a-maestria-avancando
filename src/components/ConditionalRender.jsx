6ee85842ce31e440ac3420f170d0754153513d5f
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2024-02-23 11:23:40 -0300

    If e else no JSX

--- src/components/ConditionalRender.jsx
+++ src/components/ConditionalRender.jsx
@@ -3,11 +3,23 @@ import { useState } from "react"
 
 const ConditionalRender = () => {
   const [x] = useState(true);
+  const [name, setName] = useState("Matheus");
     return (
     <div>
         <h1>Isso será exibido?</h1>
         {x && <p>Se x for true, sim!</p>}
         {!x && <p>Agora x é falso!</p>}
+        <h2>If Ternário</h2>
+        {name ==="João"? (
+            <div>
+                <p>O nome é João!</p>
+            </div>
+        ): (
+            <div>
+                <p>Nome não encontrado!</p>
+            </div>
+        )}
+        <button onClick={() => setName("João")}>Alterar o nome para João</button>
     </div>
   )
 }
